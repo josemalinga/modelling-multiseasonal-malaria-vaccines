30b6d7fd22a00d038c12be86e901a4ea1432b05b
Josephine Malinga <josephine.malinga@unibas.ch>
2024-07-23 11:06:10 +0000

    Upload New File

---
+++ bayesianoptimizationworkflow/instra.R
@@ -0,0 +1,10 @@
+
+#how to install miniconda3 to start the bayesian optimization
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+# after install
+~/miniconda3/bin/conda init bash
